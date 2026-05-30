3d0ff68fea31e3f0de2aebbbb9ba109750018b15
Hello-Kaow <kkaow99999@gmail.com>
2026-05-30 17:16:40 +0700

    865-[Fixed]resolve error

--- backend/src/prisma/migrations/20260530080618_add_bucket/migration.sql
+++ backend/src/prisma/migrations/20260530080618_add_bucket/migration.sql
@@ -1,11 +1,4 @@
 -- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL
-);
-
--- CreateTable
 CREATE TABLE "Bucket" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "title" TEXT NOT NULL,
@@ -16,6 +9,3 @@ CREATE TABLE "Bucket" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
