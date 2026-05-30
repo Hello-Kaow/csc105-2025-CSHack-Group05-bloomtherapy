35b948b783f992fcf130971094c503c17561e4b6
Sombonxaii <pansachayananth@gmail.com>
2026-05-30 09:59:05 +0700

    812 - [Create] Create Navbar.tsx

--- frontend/src/routers/routers.tsx
+++ frontend/src/routers/routers.tsx
@@ -1,15 +1,16 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import type { ReactNode } from "react";
 import Loginpage from "../pages/Loginpage";
-import Homepage from "../pages/Journal";
+//import Homepage from "../pages/Journal";
 import Signup from "../pages/Signuppage";
+import NewPageforTest from "../pages/NewPageforTest";
 
 const mainRouter = createBrowserRouter([
-    {
-        path: "/",
-        element: <Homepage/>
-    },
-    {
+    // {
+    //     path: "/",
+    //     element: <Homepage/>
+    // },
+     {
         path: "/login",
         element: <Loginpage/>
     },
@@ -17,6 +18,15 @@ const mainRouter = createBrowserRouter([
         path: "/signup",
         element: <Signup/>
     },
+    {
+        path: "/newpagefortest",
+        element: <NewPageforTest/>
+    },
+    // {
+    //     path: "/journal",
+    //     element: <Journal/>
+    // }
+
 ]);
 
 export default mainRouter;
